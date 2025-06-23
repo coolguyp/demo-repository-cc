380d86352528f2ccb6b5613303dc6efff8ef459c
coolguyp <161856663+coolguyp@users.noreply.github.com>
2025-06-23 15:29:35 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Stopping existing container if it exists..."
-docker stop flask-container || true
-docker rm flask-container || true
+# Stop the running container (if any)
+echo "Hi"
