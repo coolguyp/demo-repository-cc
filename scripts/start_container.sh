fdcc89994b5fb9a96f734d11e8418f7b1dd829e9
coolguyp <161856663+coolguyp@users.noreply.github.com>
2025-06-23 16:51:43 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the image
 docker pull pranam2835/sample-python-flask-app 
 
-# Run the Docker image as a container
-echo run -d -p 5000:5000 pranam2835/sample-python-flask-app  
+# Run the container
+docker run -d --name sample-python-flask-app -p 5000:5000 pranam2835/sample-python-flask-app
