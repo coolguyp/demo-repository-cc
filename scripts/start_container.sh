afb145a9b53f330ad7c586b3ddd5bbe4936ff3f1
coolguyp <161856663+coolguyp@users.noreply.github.com>
2025-06-23 15:03:20 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -e  # Exit on any error
-
-echo "Removing existing container if it exists..."
-docker rm -f flask-container || true
+set -e
 
 echo "Pulling Docker image..."
 docker pull pranam2835/sample-python-flask-app
 
-echo "Starting container..."
-docker run -d -p 5000:5000 --name flask-container pranam2835/sample-python-flask-app
+echo "Stopping existing containers (if any)..."
+docker stop $(docker ps -aq) || true
+docker rm $(docker ps -aq) || true
 
-echo "Deployment container started."
+echo "Starting container..."
+docker run -d -p 5000:5000 pranam2835/sample-python-flask-app
